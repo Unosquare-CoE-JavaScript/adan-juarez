35a24fe43f02165f1bb11d490deda9a44a67d2e3
cesaradan-1 <adan.juarez@unosquare.com>
2022-03-10 09:10:23 -0600

    configure the src folder which contains the rest of the course projects

---
+++ starting-project/functions.js
@@ -0,0 +1,26 @@
+"use strict";
+function addFunctionNumbers(n1, n2) {
+    return n1 + n2;
+}
+function printResults(num) {
+    const print = console.log('Result: ' + num);
+    return print;
+}
+const results = printResults(addFunctionNumbers(5, 12));
+console.log(results);
+let combineValues;
+let operationResult;
+combineValues = addFunctionNumbers;
+combineValues = printResults;
+console.log(combineValues(8, 8));
+console.log(operationResult(2, 298));
+//calbacks
+// it doesnt return anything with void, as void doesnt force to return anything
+function addAndHandle(m1, m2, cb) {
+    const result = m1 + m2;
+    cb(result);
+}
+// calback functions can return something, even if the argument on which they are passed doesn NOT expect a returned value
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+});
