d20229d12e50169cb9ecc1950c4ac3eee3bdc12f
cesaradan-1 <99829104+cesaradan-1@users.noreply.github.com>
2022-04-20 13:28:27 -0500

    fix 2

--- Async-JS/Async-1/promises/async/app1.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const asyncFunction = function() {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            resolve("Important Value")
-        }, 2000)
-    })
-}
