ff3c0bb0a453376db7a19835d0d911215599285b
cesaradan-1 <99829104+cesaradan-1@users.noreply.github.com>
2022-04-20 13:45:59 -0500

    fix 2

--- starting-project/exercise.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let userInput;
-let userNmae;
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-// The never word is indicated to never return anything
-const response = generateError('An error occurred!', 500); // --> returns never
-console.log(response);
