cd2e1d5a99262a076b03fa1bf5ba7a8fc7fc5ebe
cesaradan-1 <99829104+cesaradan-1@users.noreply.github.com>
2022-04-20 13:34:04 -0500

    re-order folder

--- middlewares/cleanCache.js
+++
@@ -1,8 +0,0 @@
-const { clearHash } = require('../services/cache');
-//receives clean cache from cache and make the function async. Delete the next in the arguments of /api/blogs
-module.exports = async (req, res, next) => {
-
-  await next();
-
-  clearHash(req.user.id);
-};
