a5c7577d70022371d45e3b24f7d6c36de813f259
cesaradan-1 <99829104+cesaradan-1@users.noreply.github.com>
2022-09-21 13:24:03 -0500

    react-developer-1

--- React-complete-developer/complete-react-advanced/src/index.js
+++ React-complete-developer/complete-react-advanced/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppMonsters, { AppMonster } from './monsters-rolodex/App';
+import App1 from './project-1/App1';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppMonster />
   </React.StrictMode>
 );
 
