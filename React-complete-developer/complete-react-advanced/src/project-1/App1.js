251f3740b740ef4fbe9c19f7b42e0ed5c3fc48ae
cesaradan-1 <99829104+cesaradan-1@users.noreply.github.com>
2022-09-26 10:58:05 -0500

    context

--- React-complete-developer/complete-react-advanced/src/project-1/App1.js
+++ React-complete-developer/complete-react-advanced/src/project-1/App1.js
@@ -1,20 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import { UserContext } from './context/user.context';
+import { useContext } from 'react';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
 };
 
 const App1 = () => {
+  const { currentUser } = useContext(UserContext)
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
+        <Route 
+          path='auth' 
+          element={ 
+            currentUser ? <Navigate to="/" replace /> : <Authentication />} 
+        />
       </Route>
     </Routes>
   );
