caedd9b595d2730e5d2c8e46777a32ac5c912537
cesaradan-1 <99829104+cesaradan-1@users.noreply.github.com>
2022-09-30 15:54:53 -0500

    finish state management

--- React-complete-developer/complete-react-advanced/src/project-1/utils/firebase/firebase.utils.js
+++ React-complete-developer/complete-react-advanced/src/project-1/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
